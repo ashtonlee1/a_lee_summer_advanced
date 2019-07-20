18898427d2c3f2a54eedfe5e182fb1c91186e9db
ashtonlee1 <alee2@collegiateschool.org>
2019-07-19 21:16:55 -0400

    hw

--- Homework/7:14:19/hw2.java
+++ Homework/7:14:19/hw2.java
@@ -5,18 +5,79 @@ public class MyLinkedList {
   public int size() {
     return size;
   }
-  public boolean add(int value) {
-    Node n = new Node();
+  public boolean add(Integer value) {
+    Node n=new Node();
     n.setData(value);
     n.next = start;
     n.prev = null;
-    if (start!=null) {
+    if(start!=null) {
       start.setPrev(n);
     }
     start = n;
+    length++;
+    return true;
   }
   public String toString() {
-
+    return("Start = " + start + " End = " + end);
+  }
+  public Integer get(int index) {
+    if(index<0||index>size()) {
+      return null;
+    }
+    Node n1 = start;
+    for(int i = 0;i<index-1;i++) {
+      n1 = n1.next();
+    }
+    return n1.getData();
+  }
+  public Integer set(int index, Integer value) {
+    if(index<0||index>size()) {
+      return null;
+  }
+    Node n=start;
+    for(int i = 0;i<index-1;i++) {
+      n=n.next();
+  }
+    return n.setData(value);
+  }
+  public boolean contains(Integer value) {
+    boolean answer = false;
+    Node n=start;
+    while(n!=null) {
+      if(n.getData()==value) {
+        answer=true;
+        break;
+      }
+      n=n.next;
+    }
+    return answer;
+  }
+  public Integer remove(int index) {
+    if(index<0||index>size()) {
+      return null;
+    }
+    Node n=start;
+    for(int i = 0;i<index-1;i++) {
+      n=n.next();
+    }
+    Integer n1 = n.getData();
+    if(n!=start) {
+      n.prev().setNext(n.next());
+    } else {
+      start=n.next();
+    }
+    if(n!=end) {
+      n.next().setPrev(n.prev());
+    } else {
+      end = n.prev();
+    }
+    if(n==start&&n==end) {
+      n.setData(null);
+      n.setPrev(null);
+      n.setNext(null);
+    }
+    length--;
+    return n1;
   }
   public class Node {
     private Node next;
@@ -28,17 +89,18 @@ public class MyLinkedList {
     public Node prev() {
       return prev;
     }
-    public void setNext() {
-      next = node;
+    public void setNext(Node other) {
+      next = other;
     }
-    public void setPrev() {
-      prev = node;
+    public void setPrev(Node other) {
+      prev = other;
     }
     public int getData() {
       return data;
     }
-    public int setData(int i) {
+    public int setData(Integer i) {
       data = i;
+      return data;
     }
     public String toString() {
       return ("Data = " + Integer.toString(data));
